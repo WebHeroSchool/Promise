0ff4d9fcec39b6c19a3f0473a4cf258d0844a377
Maxim Tyamakov <max@tyamakov.ru>
2019-09-24 14:19:10 +0300

    add changed to skript.js

--- skript.js
+++ skript.js
@@ -1,6 +1,7 @@
-date = Date();
 let body = document.body;
-
+let url = window.location.toString();
+let date = new Date();
+let requestForPromise, dateForPromise;
 function hello() {
 	setTimeout(function() {
   let preloader = document.getElementById('preloader');
@@ -10,53 +11,74 @@ function hello() {
   	preloader.classList.add('done');
     loader.classList.add('done');
   }
-  }, 2000);
+  }, 1000);
+}
+let getUsername = (url) => {
+    let splitOfUrl = url.split('=');
+    let getMyUsername = splitOfUrl[1];
+    if (getMyUsername == undefined) {
+          getMyUsername = 'SveSvet';
+    }
+    return getMyUsername
 }
 
-const name = 'SveSvet';
-const url = 'https://api.github.com/users/';
+let name = getUsername(url);
 
 const getName = new Promise((resolve, reject) => {
 	setTimeout(() => name ? resolve(name) : reject('Name not found'), 3000);
 });
 
 
-const getUrl = new Promise((resolve, reject) => {
-	setTimeout(() => url ? resolve(url) : reject('url not found'), 2000);
-});
+let getDate = new Promise((resolve, reject) => 
+setTimeout(() => date ? 
+resolve(date) : reject('Date is not defined'), 2000)
+);
 
-	Promise.all([getName, getUrl])
-  .then(([name, url]) => fetch(`${url}${name}`))
-  .then(res => res.json())
-   .then (user => {
+function dataPromise() {
+    let getInfo = fetch(`https://api.github.com/users/${getUsername(url)}`);
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(getInfo);
+            reject('error');
+        }, 3000)
+    })
+    return promise;
+}
+Promise.all([dataPromise(), getDate])
+    .then(([request, date]) => {
+    requestForPromise = request;
+    dateForPromice = date;
+})
+    .then(res => requestForPromise.json())
+    .then(user => {
     userAvatar = user.avatar_url;
     userBio = user.bio;
     userUrl = user.url;
-        
+    
+    
     if (user.id != undefined) {
-    const addUsername = () => {
+
+    let addUsername = () => {
         let user = document.createElement('h1');
-        user.innerHTML = name;
+        user.innerHTML = `${getUsername(url)}`;
         body.appendChild(user);
     }
     
-    const addBio = () => {
+    let addBio = () => {
         const bio = document.createElement('p');
         bio.innerHTML = `${userBio}`;
         body.appendChild(bio);
     }
     
-    const addAvatar = () => {
+    let addAvatar = () => {
         const avatar = document.createElement('img');
         avatar.src = this.userAvatar;
         body.appendChild(avatar);
     }
-    
     let addDate = () => {
     let dateInHTML = document.createElement('h2');
-    dateInHTML.innerHTML = date;
+    dateInHTML.innerHTML = dateForPromice;
     body.appendChild(dateInHTML);
-        
     }
     
     addUsername();
@@ -65,12 +87,9 @@ const getUrl = new Promise((resolve, reject) => {
     addDate();
     hello()
     }
-    
     else {
-        
-    alert('Вы ввели несуществующего пользователя!');
-        
+        alert('User with this name not defined')
     }
-        
-}) 
+    }) 
+
  .catch(err => alert(err + ' Профиль не найден'));
